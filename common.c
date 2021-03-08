TimothyPMann/xtrs

commit 45fc094fa8eb86bd399bf2794dee70d9216f1c79

Fill in another missing gxtrs feature.  Using a nonexistent filename in the floppy, hard drive, or stringy floppy dialog offers to create an image by that name for you.  Prior to this commit, if you said "yes" in the floppy or hard drive case, you'd get a "not implemented" error dialog and nothing would be created.  Only the stringy floppy case was implemented.  Now all cases work.

common.c
@@ -25,8 +25,13 @@
 /* Common code used by xtrs and mkdisk */
 
 #include "z80.h"
+#include "trs_disk.h"
+#include "trs_hard.h"
 #include "trs_stringy.h"
+#include "reed.h"
 #include <errno.h>
+#include <time.h>
+#include <string.h>
 
 /* Put a 2-byte quantity to a file in little-endian order */
 /* Return -1 on error, 0 otherwise */
@@ -99,8 +104,270 @@ get_fourbyte(Uint *pp, FILE* f)
 }
 
 /*
+ * Initialize a blank virtual floppy disk with specified parameters.
+ * -1 for a parameter chooses the default value.
+ * Returns negative on error.  errno will have details.
+ */
+int
+trs_disk_init_with(FILE *f,
+		   int emutype,
+		   int sides,    // emutype DMK only
+		   int density,  // emutype DMK only
+		   int eight,    // emutype DMK only
+		   int ignden)   // emutype DMK only
+{
+  int i;
+
+  if (emutype == -1) emutype = JV3;
+  if (sides == -1) sides = 2;
+  if (density == -1) density = 2;
+  if (eight == -1) eight = 0;
+  if (ignden == -1) ignden = 0;
+
+  if (sides != 1 && sides != 2) {
+    error("sides must be 1 or 2");
+    errno = EINVAL;
+    return -1;
+  }
+  if (density < 1 || density > 2) {
+    error("density must be 1 or 2");
+    errno = EINVAL;
+    return -1;
+  }
+
+  switch (emutype) {
+  case JV1:
+    /* Unformatted JV1 disk - just an empty file! */
+    break;
+
+  case JV3:
+    /* Unformatted JV3 disk. */
+    for (i=0; i<(256*34); i++) {
+      putc(0xff, f);
+    }
+    break;
+
+  case DMK:
+    /* Unformatted DMK disk */
+    putc(0, f);           /* 0: not write protected */
+    putc(0, f);           /* 1: initially zero tracks */
+    if (eight) {
+      if (density == 1)
+	i = 0x14e0;
+      else
+	i = 0x2940;
+    } else {
+      if (density == 1)
+	i = 0x0cc0;
+      else
+	i = 0x1900;
+    }
+    putc(i & 0xff, f);    /* 2: LSB of track length */
+    putc(i >> 8, f);      /* 3: MSB of track length */
+    i = 0;
+    if (sides == 1)   i |= 0x10;
+    if (density == 1) i |= 0x40;
+    if (ignden)       i |= 0x80;
+    putc(i, f);           /* 4: options */
+    putc(0, f);           /* 5: reserved */
+    putc(0, f);           /* 6: reserved */
+    putc(0, f);           /* 7: reserved */
+    putc(0, f);           /* 8: reserved */
+    putc(0, f);           /* 9: reserved */
+    putc(0, f);           /* a: reserved */
+    putc(0, f);           /* b: reserved */
+    putc(0, f);           /* c: MBZ */
+    putc(0, f);           /* d: MBZ */
+    putc(0, f);           /* e: MBZ */
+    putc(0, f);           /* f: MBZ */
+
+  default:
+    errno = EINVAL;
+    return -1;
+  }
+
+  return 0;
+}
+
+
+/*
+ * Set or clear a virtual floppy disk's internal write protect flag.
+ * Returns negative on error.  errno will have details.
+ */
+int
+trs_disk_set_write_prot(FILE *f,
+			int emutype,
+			int writeprot)
+{
+  switch (emutype) {
+  case JV1:
+    /* No indication inside; nothing to do here */
+    return 0;
+
+  case JV3:
+    /* Set the magic byte */
+    fseek(f, 256*34-1, 0);
+    return putc(writeprot ? 0 : 0xff, f);
+
+  case DMK:
+    /* Set the magic byte */
+    return putc(writeprot ? 0xff : 0, f);
+
+  default:
+    errno = EINVAL;
+    return -1;
+  }
+}
+
+/*
+ * Initialize a blank virtual hard disk with specified parameters.
+ * -1 for a parameter chooses the default value.
+ * Returns negative on error.  errno will have details.
+ */
+int
+trs_hard_init_with(FILE *f,
+		   int cyl, int sec, int gran, int dir)
+{
+  /* Blank hard disk */
+  /* We don't care about most of this header, but we generate
+     it just in case some user wants to exchange hard drives with
+     Matthew Reed's emulator or with Pete Cervasio's port of
+     xtrshard/dct to Jeff Vavasour's Model III/4 emulator.
+  */
+  time_t tt = time(0);
+  struct tm *lt = localtime(&tt);
+  Uchar *rhhp;
+  int cksum;
+  ReedHardHeader rhh;
+  int i;
+
+  if (cyl == -1) cyl = 202;
+  if (sec == -1) sec = 256;
+  if (gran == -1) gran = 8;
+  if (dir == -1) dir = 1;
+
+  if (cyl < 3) {
+    error("cyl < 3");
+    errno = EINVAL;
+    return -1;
+  }
+  if (cyl > 256) {
+    error("cyl > 256");
+    errno = EINVAL;
+    return -1;
+  }
+  if (cyl > 203) {
+    warning("cyl > 203 is incompatible with XTRSHARD/DCT");
+  }
+  if (sec < 4) {
+    error("sec < 4");
+    errno = EINVAL;
+    return -1;
+  }
+  if (sec > 256) {
+    error("sec > 256");
+    errno = EINVAL;
+    return -1;
+  }
+  if ((sec % 32) != 0) {
+    warning("(sec % 32) != 0 is incompatible with WD1000/1010 emulation");
+    if (sec > 32) {
+      warning("(sec % 32) != 0 and sec > 32 "
+	      "is incompatible with Matthew Reed's emulators");
+    }
+  }
+  if (gran < 1) {
+    error("gran < 1");
+    errno = EINVAL;
+    return -1;
+  }
+  if (gran > 8) {
+    error("gran > 8");
+    errno = EINVAL;
+    return -1;
+  }
+  if (sec < gran) {
+    error("sec < gran");
+    errno = EINVAL;
+    return -1;
+  }
+  if (sec % gran != 0) {
+    error("sec %% gran != 0");
+    errno = EINVAL;
+    return -1;
+  }
+  if (sec / gran > 32) {
+    error("sec / gran > 32");
+    errno = EINVAL;
+    return -1;
+  }
+  if (dir < 1) {
+    error("dir < 1");
+    errno = EINVAL;
+    return -1;
+  }
+  if (dir >= cyl) {
+    error("dir >= cyl");
+    errno = EINVAL;
+    return -1;
+  }
+
+  memset(&rhh, 0, sizeof(rhh));
+  rhh.id1 = 0x56;
+  rhh.id2 = 0xcb;
+  rhh.ver = 0x10;
+  rhh.cksum = 0;  /* init for cksum computation */
+  rhh.blks = 1;
+  rhh.mb4 = 4;
+  rhh.media = 0;
+  rhh.flag1 = 0;
+  rhh.flag2 = rhh.flag3 = 0;
+  rhh.crtr = 0x42;
+  rhh.dfmt = 0;
+  rhh.mm = lt->tm_mon + 1;
+  rhh.dd = lt->tm_mday;
+  rhh.yy = lt->tm_year;
+  rhh.dparm = 0;
+  rhh.cyl = cyl;
+  rhh.sec = sec;
+  rhh.gran = gran;
+  rhh.dcyl = dir;
+  strcpy(rhh.label, "xtrshard");
+
+  cksum = 0;
+  rhhp = (Uchar *) &rhh;
+  for (i=0; i<=31; i++) {
+    cksum += rhhp[i];
+  }
+  rhh.cksum = ((Uchar) cksum) ^ 0x4c;
+
+  return fwrite(&rhh, sizeof(rhh), 1, f);
+}
+
+/*
+ * Set or clear a virtual hard disk's internal write protect flag.
+ * Returns negative on error.  errno will have details.
+ */
+int
+trs_hard_set_write_prot(FILE *f, int writeprot)
+{
+  int newmode;
+
+  /* Set the magic bit */
+  fseek(f, 7, 0);
+  newmode = getc(f);
+  if (newmode == EOF) {
+    return newmode;
+  }
+  newmode = (newmode & 0x7f) | (writeprot ? 0x80 : 0);
+  fseek(f, 7, 0);
+  return putc(newmode, f);
+}
+
+
+/*
  * Initialize a blank virtual stringy floppy wafer with specified parameters.
- * Returns 0 if OK, negative on error.
+ * Returns negative on error.  errno will have details.
  */
 int
 stringy_init_with(FILE *f,
@@ -136,12 +403,17 @@ stringy_init_with(FILE *f,
 
   default:
     error("unknown wafer image type on write");
+    errno = EINVAL;
     return -1;
   }
 
   return 0;
 }
 
+/*
+ * Set or clear a virtual stringy floppy wafer's internal write protect flag.
+ * Returns negative on error.  errno will have details.
+ */
 int
 stringy_set_write_prot(FILE *f,
 		       int format,
@@ -152,13 +424,15 @@ stringy_set_write_prot(FILE *f,
 
   switch (format) {
   case STRINGY_FMT_DEBUG:
-    return ENOTSUP;
+    errno = ENOTSUP;
+    return -1;
 
   case STRINGY_FMT_ESF:
     break;
 
   default:
-    return ENOTSUP;
+    errno = EINVAL;
+    return -1;
   }
 
   ires = fseek(f, sizeof(stringy_esf_magic) + 

Makefile
@@ -43,7 +43,8 @@ CR_OBJECTS = \
 
 MD_OBJECTS = \
 	mkdisk.o \
-        common.o
+        common.o \
+	error.o
 
 HC_OBJECTS = \
 	cmd.o \

error.c
@@ -57,6 +57,20 @@ void debug(const char *fmt, ...)
   va_end(args);
 }
 
+void warning(const char *fmt, ...)
+{
+  va_list args;
+  char xfmt[2048];
+
+  strcpy(xfmt, program_name);
+  strcat(xfmt, " warning: ");
+  strcat(xfmt, fmt);
+  strcat(xfmt, "\n");
+  va_start(args, fmt);
+  vfprintf(stderr, xfmt, args);
+  va_end(args);
+}
+
 void error(const char *fmt, ...)
 {
   va_list args;

mkdisk.c
@@ -38,11 +38,12 @@
 #include <sys/stat.h>
 #include <string.h>
 #include <fcntl.h>
+#include "trs_disk.h"
+#include "trs_hard.h"
 #include "trs_stringy.h"
 
+char *program_name;
 typedef unsigned char Uchar;
-#include "reed.h"
-ReedHardHeader rhh;
 
 void Usage(char *progname)
 {
@@ -84,10 +85,19 @@ main(int argc, char *argv[])
 {
   int jv1 = 0, jv3 = 0, dmk = 0, hard = 0, wafer = 0;
   int cyl = -1, sec = -1, gran = -1, dir = -1, eight = 0, ignden = 0;
-  int writeprot = 0, unprot = 0;
-  int i, c, oumask, overwrite;
+  int writeprot = 0, unprot = 0, overwrite = 0;
+  int c, oumask, res;
   char *fname;
   FILE *f;
+
+  /* program_name must be set first because the error
+   * printing routines use it. */
+  program_name = strrchr(argv[0], '/');
+  if (program_name == NULL) {
+    program_name = argv[0];
+  } else {
+    program_name++;
+  }
 
   opterr = 0;
   for (;;) {
@@ -147,21 +157,21 @@ main(int argc, char *argv[])
 
   fname = argv[optind];
 
+  /*
+   * Media write protect/unprotect.
+   */
   if (writeprot || unprot) {
-    /* Completely different functionality here */
     struct stat st;
     int newmode;
 
     if (writeprot && unprot) {
-      fprintf(stderr,
-	      "%s: -p and -u are mutually exclusive\n", argv[0]);
+      error("-p and -u are mutually exclusive");
       exit(2);
     }
 
     if (jv1 + jv3 + dmk + hard + wafer != 1) {
-      fprintf(stderr,
-	      "%s: %s requires exactly one of -1, -3, -k, -h, or -w\n",
-	      argv[0], writeprot ? "-p" : "-u");
+      error("%s requires exactly one of -1, -3, -k, -h, or -w",
+	      writeprot ? "-p" : "-u");
       exit(2);
     }
 
@@ -183,34 +193,24 @@ main(int argc, char *argv[])
     }
 
     /* Poke inside */
-    if (jv1) {
-      /* No indication inside; nothing to do here */
-
-    } else if (jv3) {
-      /* Set the magic byte */
-      fseek(f, 256*34-1, 0);
-      putc(writeprot ? 0 : 0xff, f);
-
-    } else if (dmk) {
-      /* Set the magic byte */
-      putc(writeprot ? 0xff : 0, f);
-
-    } else if (hard) {
-      /* Set the magic bit */
-      fseek(f, 7, 0);
-      newmode = getc(f);
-      if (newmode == EOF) {
+    if (jv1 || jv3 || dmk) {
+      res = trs_disk_set_write_prot(f, jv1 ? JV1 : jv3 ? JV3 : DMK, writeprot);
+      if (res < 0) {
 	perror(fname);
 	exit(1);
       }
-      newmode = (newmode & 0x7f) | (writeprot ? 0x80 : 0);
-      fseek(f, 7, 0);
-      putc(newmode, f);
+
+    } else if (hard) {
+      res = trs_hard_set_write_prot(f, writeprot);
+      if (res < 0) {
+	perror(fname);
+	exit(1);
+      }
 
     } else if (wafer) {
       /* Set the magic bit */
-      newmode = stringy_set_write_prot(f, STRINGY_FMT_ESF, writeprot);
-      if (newmode < 0) {
+      res = stringy_set_write_prot(f, STRINGY_FMT_ESF, writeprot);
+      if (res < 0) {
 	perror(fname);
 	exit(1);
       }
@@ -233,6 +233,9 @@ main(int argc, char *argv[])
     exit(0);
   }
 
+  /*
+   * Media creation
+   */
   switch (jv1 + jv3 + dmk + hard + wafer) {
   case 0:
     jv3 = 1;
@@ -240,211 +243,39 @@ main(int argc, char *argv[])
   case 1:
     break;
   default:
-    fprintf(stderr,
-	    "%s: -1, -3, -k, -h, and -w are mutually exclusive\n", argv[0]);
+    error("-1, -3, -k, -h, and -w are mutually exclusive");
     exit(2);
   }
 
   if ((jv1 || jv3) && (cyl >= 0 || sec >= 0 || gran >= 0 || dir >= 0)) {
-    fprintf(stderr,
-	    "%s: -c, -s, -g, -d are not meaningful with -1 or -3\n", argv[0]);
+    error("-c, -s, -g, -d are not meaningful with -1 or -3");
     exit(2);
   }
 
   if (dmk && (cyl >= 0 || gran >= 0)) {
-    fprintf(stderr, "%s: -c and -g are not meaningful with -k\n", argv[0]);
+    error("-c and -g are not meaningful with -k");
     exit(2);
   }
 
   if (!dmk && (eight || ignden)) {
-    fprintf(stderr, "%s: -8 and -i are only meaningful with -k\n", argv[0]);
+    error("-8 and -i are only meaningful with -k");
     exit(2);
   }
 
-  if (jv1) {
-    /* Unformatted JV1 disk - just an empty file! */
-    f = fopen_w(fname, overwrite);
-    if (f == NULL) {
-      perror(fname);
-      exit(1);
-    }
+  f = fopen_w(fname, overwrite);
+  if (f == NULL) {
+    perror(fname);
+    exit(1);
+  }
 
-  } else if (jv3) {
-    /* Unformatted JV3 disk. */
-    f = fopen_w(fname, overwrite);
-    if (f == NULL) {
-      perror(fname);
-      exit(1);
-    }
-    for (i=0; i<(256*34); i++) {
-      putc(0xff, f);
-    }
-
-  } else if (dmk) {
-    /* Unformatted DMK disk */
+  if (jv1 || jv3 || dmk) {
     /* Reuse flag letters s, d */
 #define sides sec
 #define density dir
-    if (sides == -1) sides = 2;
-    if (density == -1) density = 2;
-
-    if (sides != 1 && sides != 2) {
-      fprintf(stderr, "%s error: sides must be 1 or 2\n", argv[0]);
-      exit(2);
-    }
-    if (density < 1 || density > 2) {
-      fprintf(stderr, "%s error: density must be 1 or 2\n", argv[0]);
-      exit(2);
-    }
-
-    f = fopen_w(fname, overwrite);
-    if (f == NULL) {
-      perror(fname);
-      exit(1);
-    }
-    putc(0, f);           /* 0: not write protected */
-    putc(0, f);           /* 1: initially zero tracks */
-    if (eight) {
-      if (density == 1)
-	i = 0x14e0;
-      else
-	i = 0x2940;
-    } else {
-      if (density == 1)
-	i = 0x0cc0;
-      else
-	i = 0x1900;
-    }
-    putc(i & 0xff, f);    /* 2: LSB of track length */
-    putc(i >> 8, f);      /* 3: MSB of track length */
-    i = 0;
-    if (sides == 1)   i |= 0x10;
-    if (density == 1) i |= 0x40;
-    if (ignden)       i |= 0x80;
-    putc(i, f);           /* 4: options */
-    putc(0, f);           /* 5: reserved */
-    putc(0, f);           /* 6: reserved */
-    putc(0, f);           /* 7: reserved */
-    putc(0, f);           /* 8: reserved */
-    putc(0, f);           /* 9: reserved */
-    putc(0, f);           /* a: reserved */
-    putc(0, f);           /* b: reserved */
-    putc(0, f);           /* c: MBZ */
-    putc(0, f);           /* d: MBZ */
-    putc(0, f);           /* e: MBZ */
-    putc(0, f);           /* f: MBZ */
-
+    res = trs_disk_init_with(f, jv1 ? JV1 : jv3 ? JV3 : DMK,
+			     sides, density, eight, ignden);
   } else if (hard) {
-    /* Unformatted hard disk */
-    /* We don't care about most of this header, but we generate
-       it just in case some user wants to exchange hard drives with
-       Matthew Reed's emulator or with Pete Cervasio's port of
-       xtrshard/dct to Jeff Vavasour's Model III/4 emulator.
-    */
-    time_t tt = time(0);
-    struct tm *lt = localtime(&tt);
-    Uchar *rhhp;
-    int cksum;
-
-    if (cyl == -1) cyl = 202;
-    if (sec == -1) sec = 256;
-    if (gran == -1) gran = 8;
-    if (dir == -1) dir = 1;
-
-    if (cyl < 3) {
-      fprintf(stderr, "%s error: cyl < 3\n", argv[0]);
-      exit(2);
-    }
-    if (cyl > 256) {
-      fprintf(stderr, "%s error: cyl > 256\n", argv[0]);
-      exit(2);
-    }
-    if (cyl > 203) {
-      fprintf(stderr,
-	      "%s warning: cyl > 203 is incompatible with XTRSHARD/DCT\n",
-	      argv[0]);
-    }
-    if (sec < 4) {
-      fprintf(stderr, "%s error: sec < 4\n", argv[0]);
-      exit(2);
-    }
-    if (sec > 256) {
-      fprintf(stderr, "%s error: sec > 256\n", argv[0]);
-      exit(2);
-    }
-    if ((sec % 32) != 0) {
-      fprintf(stderr, "%s warning: %s\n", argv[0],
-	      "(sec % 32) != 0 is incompatible with WD1000/1010 emulation");
-      if (sec > 32) {
-	fprintf(stderr, "%s warning: %s\n", argv[0],
-		"(sec % 32) != 0 and sec > 32 "
-		"is incompatible with Matthew Reed's emulators");
-      }
-    }
-    if (gran < 1) {
-      fprintf(stderr, "%s error: gran < 1\n", argv[0]);
-      exit(2);
-    }
-    if (gran > 8) {
-      fprintf(stderr, "%s error: gran > 8\n", argv[0]);
-      exit(2);
-    }
-    if (sec < gran) {
-      fprintf(stderr, "%s error: sec < gran\n", argv[0]);
-      exit(2);
-    }
-    if (sec % gran != 0) {
-      fprintf(stderr, "%s error: sec %% gran != 0\n", argv[0]);
-      exit(2);
-    }
-    if (sec / gran > 32) {
-      fprintf(stderr, "%s error: sec / gran > 32\n", argv[0]);
-      exit(2);
-    }
-    if (dir < 1) {
-      fprintf(stderr, "%s error: dir < 1\n", argv[0]);
-      exit(2);
-    }
-    if (dir >= cyl) {
-      fprintf(stderr, "%s error: dir >= cyl\n", argv[0]);
-      exit(2);
-    }
-
-    memset(&rhh, 0, sizeof(rhh));
-    rhh.id1 = 0x56;
-    rhh.id2 = 0xcb;
-    rhh.ver = 0x10;
-    rhh.cksum = 0;  /* init for cksum computation */
-    rhh.blks = 1;
-    rhh.mb4 = 4;
-    rhh.media = 0;
-    rhh.flag1 = 0;
-    rhh.flag2 = rhh.flag3 = 0;
-    rhh.crtr = 0x42;
-    rhh.dfmt = 0;
-    rhh.mm = lt->tm_mon + 1;
-    rhh.dd = lt->tm_mday;
-    rhh.yy = lt->tm_year;
-    rhh.dparm = 0;
-    rhh.cyl = cyl;
-    rhh.sec = sec;
-    rhh.gran = gran;
-    rhh.dcyl = dir;
-    strcpy(rhh.label, "xtrshard");
-
-    cksum = 0;
-    rhhp = (Uchar *) &rhh;
-    for (i=0; i<=31; i++) {
-      cksum += rhhp[i];
-    }
-    rhh.cksum = ((Uchar) cksum) ^ 0x4c;
-
-    f = fopen_w(fname, overwrite);
-    if (f == NULL) {
-      perror(fname);
-      exit(1);
-    }
-    fwrite(&rhh, sizeof(rhh), 1, f);
+    res = trs_hard_init_with(f, cyl, sec, gran, dir);
 
   } else /* wafer */ {
     /* Reuse flag letter s */
@@ -454,16 +285,18 @@ main(int argc, char *argv[])
     } else {
       size = stringy_len_from_kb(size);
     }
+    res = stringy_init_with(f, STRINGY_FMT_DEFAULT, size,
+			    STRINGY_EOT_DEFAULT, FALSE);
+  }
 
-    f = fopen_w(fname, overwrite);
-    if (f == NULL) {
-      perror(fname);
-      exit(1);
-    }
-    stringy_init_with(f, STRINGY_FMT_DEFAULT, size,
-		      STRINGY_EOT_DEFAULT, FALSE);
+  if (res < 0) {
+    perror(fname);
+    fclose(f);
+    unlink(fname);
+    exit(1);
   }
 
   fclose(f);
+
   return 0;
 }

trs_disk.c
@@ -229,13 +229,6 @@ typedef struct {
 
 #define JV1_SECPERTRK 10
 
-/* Values for emulated disk image type (emutype) below */
-#define JV1 1 /* compatible with Vavasour Model I emulator */
-#define JV3 3 /* compatible with Vavasour Model III/4 emulator */
-#define DMK 4 /* compatible with Keil Model III/4 emulator */
-#define REAL 100 /* real floppy drive, PC controller */
-#define NONE 0
-
 typedef struct {
   int free_id[4];		  /* first free id, if any, of each size */
   int last_used_id;		  /* last used index */
@@ -917,8 +910,23 @@ trs_disk_set_name(int drive, const char *name)
 int
 trs_disk_create(const char *name)
 {
-  error("XXX not implemented yet; use mkdisk");
-  return ENOTSUP;
+  FILE *f;
+  int ires;
+
+  f = fopen(name, "w");
+  if (f == NULL) {
+      return errno;
+  }
+
+  /*
+   * Default parameters
+   */
+  ires = trs_disk_init_with(f, -1, -1, -1, -1, -1);
+  if (ires < 0) return errno;
+
+  ires = fclose(f);
+  if (ires < 0) return errno;
+  return 0;
 }
 
 static int

trs_disk.h
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright (c) 1996-2020, Timothy P. Mann
  *
  * Permission is hereby granted, free of charge, to any person
@@ -45,10 +45,21 @@ int trs_disk_getstep(int unit);
 void trs_disk_setsize(int unit, int value);
 int trs_disk_getsize(int unit);
 
+int trs_disk_init_with(FILE *f, int emutype,
+		       int sides, int density, int eight, int ignden);
+int trs_disk_set_write_prot(FILE *f, int emutype, int writeprot);
+
 extern int trs_disk_doubler;
 extern char* trs_disk_dir;
 extern unsigned short trs_changecount;
 extern int trs_disk_truedam;
+
+/* Values for emulated disk image type (emutype) */
+#define JV1 1 /* compatible with Vavasour Model I emulator */
+#define JV3 3 /* compatible with Vavasour Model III/4 emulator */
+#define DMK 4 /* compatible with Keil Model III/4 emulator */
+#define REAL 100 /* real floppy drive, PC controller */
+#define NONE 0
 
 /* Values for trs_disk_doubler flag word */
 #define TRSDISK_NODOUBLER 0
@@ -78,7 +89,7 @@ extern int trs_disk_truedam;
 #define TRSDISK_DATA    0x37ef
 
 /* FDC port space in Model III */
-#define TRSDISK3_INTERRUPT 0xe4 
+#define TRSDISK3_INTERRUPT 0xe4
 #define TRSDISK3_COMMAND   0xf0  /* writing */
 #define TRSDISK3_STATUS    0xf0  /* reading */
 #define TRSDISK3_TRACK     0xf1

trs_hard.c
@@ -135,8 +135,23 @@ trs_hard_set_name(int drive, const char *name)
 int
 trs_hard_create(const char *name)
 {
-  error("XXX not implemented yet; use mkdisk");
-  return ENOTSUP;
+  FILE *f;
+  int ires;
+
+  f = fopen(name, "w");
+  if (f == NULL) {
+      return errno;
+  }
+
+  /*
+   * Default parameters
+   */
+  ires = trs_hard_init_with(f, -1, -1, -1, -1);
+  if (ires < 0) return errno;
+
+  ires = fclose(f);
+  if (ires < 0) return errno;
+  return 0;
 }
 
 /* Powerup or reset button */

trs_hard.h
@@ -39,6 +39,9 @@ int trs_hard_set_name(int drive, const char *name);
 int trs_hard_create(const char *name);
 int trs_hard_in(int port);
 void trs_hard_out(int port, int value);
+int trs_hard_init_with(FILE *f, int cyl, int sec, int gran, int dir);
+int trs_hard_set_write_prot(FILE *f, int writeprot);
+
 extern char *trs_disk_dir;
 
 /* Sector size is always 256 for TRSDOS/LDOS/etc. */

commit f95b590a444e414f3c7b0f262c2702fe83aed928

Fix missing case break that caused incorrect error returns from blank DMK virtual media.  This was a regression in commit 45fc094f.

common.c
@@ -180,6 +180,7 @@ trs_disk_init_with(FILE *f,
     putc(0, f);           /* d: MBZ */
     putc(0, f);           /* e: MBZ */
     putc(0, f);           /* f: MBZ */
+    break;
 
   default:
     errno = EINVAL;