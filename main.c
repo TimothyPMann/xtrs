TimothyPMann/xtrs

commit 6450b7f685b520bfd2cb3ef22443dcfb986de233

Partial cleanup of ROM loading
compile_rom is generalized a bit to deal better with ROMs that don't
start at address 0.  This avoids wasting 12KB of space when compiling
in the ESF extension ROM, which starts at 0x3000.

main.c is changed to work with the improvement, but doesn't take full
advantage; it continues to assume the ROMs are located where they are
supposed to be and will break silently if they don't.  I ran out of
time to do a more thorough cleanup.

main.c
@@ -70,14 +70,21 @@ static void check_endian(void)
 }
 
 /*
- * Note: If there are multiple ROMs, trs_rom_size is set to the ending
- * address(+1) of the last ROM loaded.  So ROMs must be loaded in
- * ascending order by address.
+ * Although this ROM loading code supports multiple ROMs, xtrs has an
+ * overall assumption that addresses from 0 up to the end of the
+ * highest-addressed ROM (if any) are all ROM space.  So we set
+ * trs_rom_size to the end of the highest-addressed ROM that has been
+ * seen.  Moreover, we assume we know where the ROMs start -- address
+ * 0 for all except the Model I ESF extension ROM, which starts at
+ * 0x3000.  We don't check if this assumption is violated even in
+ * cases where it's possible to check, such as when the ROM is in
+ * Intel hex format or load module format.
  */
 void trs_load_rom(int address, char *filename)
 {
     FILE *program;
     int c;
+    int rom_end = 0;
 
     if((program = fopen(filename, "r")) == NULL)
     {
@@ -90,51 +97,59 @@ void trs_load_rom(int address, char *filename)
     if (c == ':') {
         /* Assume Intel hex format */
         rewind(program);
-        trs_rom_size = load_hex(program);
+        rom_end = load_hex(program);
 	fclose(program);
 	return;
     }
 
     if (c == 1 || c == 5) {
-        /* Assume MODELA/III file */
+        /* Assume MODELA/III file (load module) */
 	int res;
 	extern Uchar *rom; /*!! fixme*/
 	Uchar loadmap[Z80_ADDRESS_LIMIT];
 	rewind(program);
 	res = load_cmd(program, rom, loadmap, 0, NULL, -1, NULL, NULL, 1);
 	if (res == LOAD_CMD_OK) {
-	    trs_rom_size = Z80_ADDRESS_LIMIT;
-	    while (trs_rom_size > 0) {
-		if (loadmap[--trs_rom_size] != 0) {
-		    trs_rom_size++;
+	    rom_end = Z80_ADDRESS_LIMIT;
+	    while (rom_end > 0) {
+		if (loadmap[--rom_end] != 0) {
+		    rom_end++;
 		    break;
 		}
 	    }
 	    fclose(program);
 	    return;
 	} else {
-	    /* Apparently it wasn't one */
+	    /* Apparently it wasn't one; prepare to fall through to
+             * raw binary case. */
 	    rewind(program);
 	    c = getc(program);
 	}
     }
 
     /* Assume raw binary */
-    trs_rom_size = address;
+    rom_end = address;
     while (c != EOF) {
-        mem_write_rom(trs_rom_size++, c);
+        mem_write_rom(rom_end++, c);
 	c = getc(program);
     }
     fclose(program);
+
+    if (rom_end > trs_rom_size) {
+       trs_rom_size = rom_end;
+    }
 }
 
 void trs_load_compiled_rom(int address, int size, unsigned char rom[])
 {
     int i;
-    
-    trs_rom_size = size;
-    for (i = address; i < size; i++) {
-	mem_write_rom(i, rom[i]);
+
+    for (i = 0; i < size; i++) {
+	mem_write_rom(address + i, rom[i]);
+    }
+
+    if (address + size > trs_rom_size) {
+       trs_rom_size = address + size;
     }
 }
 

compile_rom.c
@@ -41,6 +41,7 @@
 #include "load_cmd.h"
 
 char *program_name;
+static int lowest_address = Z80_ADDRESS_LIMIT;
 static int highest_address = 0;
 static Uchar memory[Z80_ADDRESS_LIMIT];
 static Uchar loadmap[Z80_ADDRESS_LIMIT];
@@ -51,7 +52,9 @@ void hex_data(int address, int value)
   address &= 0xffff;
 
   memory[address] = value;
-  if(highest_address < address)
+  if (lowest_address > address)
+    lowest_address = address;
+  if (highest_address < address)
     highest_address = address;
 }
 
@@ -64,13 +67,12 @@ static void load_rom(char *filename)
 {
   FILE *program;
   int c, a;
-    
-  if((program = fopen(filename, "r")) == NULL)
-    {
-      char message[100];
-      sprintf(message, "could not read %s", filename);
-      fatal(message);
-    }
+
+  if ((program = fopen(filename, "r")) == NULL) {
+    char message[100];
+    sprintf(message, "could not read %s", filename);
+    fatal(message);
+  }
   c = getc(program);
   if (c == ':') {
     /* Assume Intel hex format */
@@ -79,11 +81,18 @@ static void load_rom(char *filename)
     fclose(program);
     return;
   } else if (c == 1 || c == 5) {
-    /* Assume MODELA/III file */
+    /* Assume MODELA/III file (load module format) */
     int res;
     rewind(program);
     res = load_cmd(program, memory, loadmap, 0, NULL, -1, NULL, NULL, 1);
     if (res == LOAD_CMD_OK) {
+      lowest_address = 0;
+      while (lowest_address < Z80_ADDRESS_LIMIT) {
+        if (loadmap[lowest_address] != 0) {
+          break;
+        }
+        lowest_address++;
+      }
       highest_address = Z80_ADDRESS_LIMIT;
       while (highest_address > 0) {
 	if (loadmap[--highest_address] != 0) {
@@ -93,11 +102,13 @@ static void load_rom(char *filename)
       fclose(program);
       return;
     } else {
-      /* Guess it wasn't one */
+      /* Guess it wasn't a load module; prepare to fall into raw
+       * binary case */
       rewind(program);
       c = getc(program);
-    } 
+    }
   }
+  /* Assume raw binary at address 0 */
   a = 0;
   while (c != EOF) {
     hex_data(a++, c);
@@ -110,29 +121,28 @@ static void write_output(char *which)
 {
   int address = 0;
   int i;
-    
-  highest_address++;
+  int size = highest_address - lowest_address + 1;
 
-  printf("int trs_rom%s_size = %d;\n", which, highest_address);
-  printf("unsigned char trs_rom%s[%d] = \n{\n", which, highest_address);
-    
-  while(address < highest_address) 
-    {
-      printf("    ");
-      for(i = 0; i < 8; ++i)
-	{
-	  printf("0x%.2x,", memory[address++]);
+  printf("int trs_rom%s_start = %d;\n", which, lowest_address);
+  printf("int trs_rom%s_size = %d;\n", which, size);
+  printf("unsigned char trs_rom%s[%d] = \n{\n", which, size);
 
-	  if(address == highest_address)
-	    break;
-	}
-      printf("\n");
+  address = lowest_address;
+  while (address <= highest_address) {
+    printf("    ");
+    for (i = 0; i < 8; ++i) {
+      printf("0x%.2x,", memory[address++]);
+      if (address > highest_address)
+        break;
     }
+    printf("\n");
+  }
   printf("};\n");
 }
 
 static void write_norom_output(char *which)
 {
+  printf("int trs_rom%s_start = -1;\n", which);
   printf("int trs_rom%s_size = -1;\n", which);
   printf("unsigned char trs_rom%s[1];\n", which);
 }
@@ -140,21 +150,16 @@ static void write_norom_output(char *which)
 int main(int argc, char *argv[])
 {
   program_name = argv[0];
-  if(argc == 2)
-    {
-      fprintf(stderr,
-        "%s: no specified ROM file, ROM %s will not be built into program.\n",
-	program_name, argv[1]);
-      write_norom_output(argv[1]);
-    }
-  else if(argc != 3)
-    {
-      fprintf(stderr, "Usage: %s model hexfile\n", program_name);
-    }
-  else
-    {
-      load_rom(argv[2]);
-      write_output(argv[1]);
-    }
+  if (argc == 2) {
+    fprintf(stderr,
+      "%s: no specified ROM file, ROM %s will not be built into program.\n",
+            program_name, argv[1]);
+    write_norom_output(argv[1]);
+  } else if(argc != 3) {
+    fprintf(stderr, "Usage: %s model hexfile\n", program_name);
+  } else {
+    load_rom(argv[2]);
+    write_output(argv[1]);
+  }
   return 0;
 }

commit 243a6b97f0df817cc47cfa5f21a3d2b57e8404b2

Fix ROM loading from files
Fix bugs introduced in 6450b7f6

main.c
@@ -98,8 +98,7 @@ void trs_load_rom(int address, char *filename)
         /* Assume Intel hex format */
         rewind(program);
         rom_end = load_hex(program);
-	fclose(program);
-	return;
+	goto done;
     }
 
     if (c == 1 || c == 5) {
@@ -117,8 +116,7 @@ void trs_load_rom(int address, char *filename)
 		    break;
 		}
 	    }
-	    fclose(program);
-	    return;
+	    goto done;
 	} else {
 	    /* Apparently it wasn't one; prepare to fall through to
              * raw binary case. */
@@ -133,8 +131,9 @@ void trs_load_rom(int address, char *filename)
         mem_write_rom(rom_end++, c);
 	c = getc(program);
     }
-    fclose(program);
 
+ done:
+    fclose(program);
     if (rom_end > trs_rom_size) {
        trs_rom_size = rom_end;
     }